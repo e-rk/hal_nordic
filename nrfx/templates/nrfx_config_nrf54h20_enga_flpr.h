79d68634f28178565da622a9ca6c50f9c7300d04
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2024-10-10 10:10:20 +0200

    preview v2

--- nrfx/templates/nrfx_config_nrf54h20_enga_flpr.h
+++ nrfx/templates/nrfx_config_nrf54h20_enga_flpr.h
@@ -294,8 +294,35 @@
  *
  * Boolean. Accepted values: 0 and 1.
  */
-#ifndef NRFX_GRTC_CONFIG_SLEEP_ALLOWED
-#define NRFX_GRTC_CONFIG_SLEEP_ALLOWED 0
+#ifndef NRFX_DPPI134_ENABLED
+#define NRFX_DPPI134_ENABLED 0
+#endif
+
+/**
+ * @brief NRFX_DPPI135_ENABLED
+ *
+ * Boolean. Accepted values: 0 and 1.
+ */
+#ifndef NRFX_DPPI135_ENABLED
+#define NRFX_DPPI135_ENABLED 0
+#endif
+
+/**
+ * @brief NRFX_DPPI136_ENABLED
+ *
+ * Boolean. Accepted values: 0 and 1.
+ */
+#ifndef NRFX_DPPI136_ENABLED
+#define NRFX_DPPI136_ENABLED 0
+#endif
+
+/**
+ * @brief NRFX_GRTC_ENABLED
+ *
+ * Boolean. Accepted values: 0 and 1.
+ */
+#ifndef NRFX_GRTC_ENABLED
+#define NRFX_GRTC_ENABLED 0
 #endif
 
 /**
